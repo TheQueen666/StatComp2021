0d71a3f596a0a478dfe4d6919dbed29f0c55b4ca
Gabriele Giordano <contact.ggiordano@gmail.com>
2021-11-15 18:22:37 +0100

    a

--- StatComp2021.R
+++ StatComp2021.R
@@ -41,13 +41,17 @@ cor(dati[,4:30])
 cor(dati[,c("gen_health","iron")]) #controllo per gen_health, iron 
 # troppi NA?
 
-
+### Modifico variabili ----
+dati$gender<-dati$gender%>%as.factor
+dati$education<-dati$education%>%as.factor
+dati$marital<-dati$marital%>%as.factor
+dati$gen_health<-dati$gen_health%>%as.factor
 
 ### First model ----
 # dalle descrittive osserviamo che Y non si distribuisce come una normale
 # trasformeremo dopo
 colnames(dati)
-lm1<-lm(drinks_day~diabetes+depression
+lm1<-lm(drinks_day~diabetes+as.factor(depression)
         +age+gender+race+grip_strength  
         +education+bmi+marital+income+household_size
         +insurance
@@ -58,6 +62,8 @@ summary(lm1)
 par(mfrow=c(2,2)) 
 plot(lm1)
 par(mfrow=c(1,1)) 
+
+p<-predict(lm1,dati)
 
 ### Controllo collinearità con TOL sotto 0,3 vengolo tolte -----
 target=dati[,c("drinks_day")]
@@ -118,7 +124,7 @@ anova(lm2,lmr) # non conviene usare l'ultimo modello
 library(gam)
 gam1<-gam(drinks_day~0+
           +s(age)+gender+s(race)+s(grip_strength)  
-          +s(education)+s(bmi)+s(marital)+s(income)+s(household_size)
+          +s(education)+s(bmi)+s(marital)+s(household_size)
           +insurance
           #+private_insur+medicare+medicaid
           +s(gen_health)+s(iron), data=dati)
@@ -127,26 +133,69 @@ summary(gam1)
 # education vuole un terzo grado o parabola inferzo, bmi forse una parabola o una terza
 # income una esponenziale
 
-par(mfrow=c(2,2)) 
-plot(gam1)
-par(mfrow=c(1,1)) #la data sembra suggerire un log
+### Modello con trasformate ma modificato----
+gam2<-gam(log(drinks_day+1)~0
+                  +s(age)+gender+s(race) 
+          +s(education)+s(bmi)+s(marital)+s(household_size)
+          +insurance
+          #+private_insur+medicare+medicaid
+          +s(gen_health)+s(iron), data=dati)
+summary(gam2)
 
-### Ultimo modello  da rivedere----
+
+par(mfrow=c(2,2)) 
+plot(gam2)
+par(mfrow=c(1,1)) 
+# cosa passa da age?
+# race una parabola
+# Education una parabola
+
+### Ultimo modello: metto factor delle variabili e uso GAM---
 lmf<-lm(log(drinks_day+1)~0
-        +age+gender
-        #+ race^2
-                +education^3+bmi^3+marital+exp(income)
+        +age+factor(gender)+as.factor(race)
+        +income
+        +factor(education)+bmi+factor(marital)
         #+insurance+private_insur+medicare+medicaid
         +gen_health+iron, data=dati)
 summary(lmf)
-# guadagno risibile rispetto al secondo modello 
+
+shapiro.test(lmf$residuals) #accetta normalità dei residui
+
+# guadagno risibile rispetto al secondo modello: conviene usare lm2
 
 par(mfrow=c(2,2)) 
 plot(lmf)
 par(mfrow=c(1,1)) 
 
-car::ncvTest(lmf) #permane eteros.
+car::ncvTest(lmf) #permane eteros. In ogni caso uso lm2
 
+# Controllo ipotesi
+library(gvlma)
+library(lmtest)
+#Prima
+bptest(lm1)
+gvlma(lm1)
+
+#Dopo
+bptest(lmf)
+gvlma(lmf) 
 
 
 ### logistico: drinkdays >= 52 come soglia critica ----
+table(dati$drinks_day); median(dati$drinks_day)
+drink_dangerous<-ifelse((dati$drinks_day)>11,1,0);
+glm1<-glm(drink_dangerous~0
+          +age+gender
+          #+ race^2
+          +education^3+bmi^3+marital+exp(income)
+          #+insurance+private_insur+medicare+medicaid
+          +gen_health+iron, data=dati)
+
+summary(glm1)
+exp(glm1$coefficients)
+
+### Confronto modello iniziale-finale ----
+pf<-predict(lmf,dati)
+par(mfrow=c(1,2)) 
+plot(p,dati$drinks_day)
+plot(pf, dati$drinks_day)
