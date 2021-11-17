370fc74c09c80ceb41555cf3220494c42a127ff4
Gabriele Giordano <contact.ggiordano@gmail.com>
2021-11-17 11:40:09 +0100

    17/11

--- StatComp2021.R
+++ StatComp2021.R
@@ -14,6 +14,9 @@ library(ggplot2)
 library(tidyverse)
 library(mctest)
 library("skimr")
+library(gvlma)
+library(lmtest)
+library(sandwich)
 options(scipen=999)
 
 ### Descriptives ----
@@ -50,10 +53,10 @@ dati$gen_health<-dati$gen_health%>%as.factor
 ### First model ----
 # dalle descrittive osserviamo che Y non si distribuisce come una normale
 # trasformeremo dopo
-colnames(dati)
+formula <- paste(colnames(dati), collapse="','")
 lm1<-lm(drinks_day~diabetes+as.factor(depression)
         +age+gender+race+grip_strength  
-        +education+bmi+marital+income+household_size
+        +education+bmi+marital+income+as.factor(household_size)
         +insurance
         #+private_insur+medicare+medicaid
         +gen_health+iron, data=dati) 
@@ -63,7 +66,11 @@ par(mfrow=c(2,2))
 plot(lm1)
 par(mfrow=c(1,1)) 
 
-p<-predict(lm1,dati)
+bptest(lm1) #presenta eteros. serve sistemare l'inferenza.
+coeftest(lm1, vcov=vcovHC(lm1)) 
+
+
+p<-predict(lm1,dati) #serve dopo
 
 ### Controllo collinearità con TOL sotto 0,3 vengolo tolte -----
 target=dati[,c("drinks_day")]
@@ -77,7 +84,7 @@ imcdiag(lm1) # funziona
 ### Second model ----
 # trasformo Y tramite log, tolgo variabili non significative
 # tolgo intercetta, non ha senso
-hist(log(dati$drinks_day+1)) #esponenziale negativissima
+hist(dati$drinks_day) #esponenziale negativissima
 lm2<-lm(log(drinks_day+1)~0
         +age+gender
         +education+bmi+marital
@@ -89,6 +96,9 @@ par(mfrow=c(2,2))
 plot(lm2)
 par(mfrow=c(1,1)) 
 
+bptest(lm2) #presenta eteros. serve sistemare l'inferenza.
+coeftest(lm2, vcov=vcovHC(lm2)) 
+
 imcdiag(lm2)
 
 
@@ -98,8 +108,38 @@ plot(p, dati$drinks_day) #brutto
 car::ncvTest(lm2) # permane eteroschedasticità
 
 
-### Osservazioni influenti? -----
-### Distanze di cook valori influenti----
+### Osservazioni influenti? Codice lovaglio -----
+# outliers
+library(car)
+influencePlot(lm2,  main="Influence Plot", sub="Circle size is proportial to Cook's Distance" )
+
+cooksd <- cooks.distance(lm2)
+cooksda=data.frame(cooksd)
+
+# cutoff of cookD  4/(n-k).. NB n should be n used in the model!!!
+
+n_used=length(lm2$residuals)
+n_used # be careful!!! 
+
+cutoff <- 4/(n_used-length(lm2$coefficients)-2)
+cutoff
+Noinflu=data.frame(dati[cooksd < cutoff, ])  # influential row numbers
+
+lminf = lm(log(drinks_day+1)~0
+           +age+gender
+           +education+bmi+marital
+           #+insurance+private_insur+medicare+medicaid
+           +gen_health+iron, data=dati)
+summary(lminf)
+
+par(mfrow=c(2,2)) 
+plot(lminf)
+par(mfrow=c(1,1)) 
+
+bptest(lminf) #presenta eteros. serve sistemare l'inferenza.
+# Togliere osservazioni non migliora il modello
+
+### Distanze di cook valori influenti: versione Pennoni ----
 require(faraway)
 cook <- cooks.distance(lm1)
 halfnorm(cook, ylab = "distanza di Cook") # 1,3
@@ -120,35 +160,64 @@ par(mfrow=c(1,1))
 
 anova(lm2,lmr) # non conviene usare l'ultimo modello 
 
-### Modello con trasformate ----
-library(gam)
-gam1<-gam(drinks_day~0+
-          +s(age)+gender+s(race)+s(grip_strength)  
-          +s(education)+s(bmi)+s(marital)+s(household_size)
-          +insurance
-          #+private_insur+medicare+medicaid
-          +s(gen_health)+s(iron), data=dati)
-summary(gam1)
-# race  vuole una parabola probabilmente
-# education vuole un terzo grado o parabola inferzo, bmi forse una parabola o una terza
-# income una esponenziale
+###  Inferenza con stand err robusti: da rivedere ----
 
-### Modello con trasformate ma modificato----
-gam2<-gam(log(drinks_day+1)~0
-                  +s(age)+gender+s(race) 
-          +s(education)+s(bmi)+s(marital)+s(household_size)
-          +insurance
-          #+private_insur+medicare+medicaid
-          +s(gen_health)+s(iron), data=dati)
-summary(gam2)
 
+
+coeftest(lm2, vcov=vcovHC(lm2)) 
+
+### Box Cox----
+library(MASS)
+boxcoxreg1<-boxcox(lm2)
+title("Lambda")
+lambda=boxcoxreg1$x[which.max(boxcoxreg1$y)]
+lambda # suggerisce trasformata y -0.3434343
+
+dati$drinks_day_modificati<-dati$drinks_day^-0.3434343
+lmc<-lm(drinks_day_modificati~0
+        +age+gender+ 
+                +education+bmi+marital
+        #+insurance+private_insur+medicare+medicaid
+        +gen_health+iron, data=dati) 
+summary(lmc) #netto miglioramento
 
 par(mfrow=c(2,2)) 
-plot(gam2)
+plot(lmc)
 par(mfrow=c(1,1)) 
-# cosa passa da age?
-# race una parabola
-# Education una parabola
+
+bptest(lmc) #permane eteros
+
+
+### Modello con trasformate ----
+library(gam)
+gam1<-gam(drinks_day_modificati~0
+          +s(age)+gender+ 
+                  +education+s(bmi)+marital
+          +gen_health+s(iron), data=dati)
+summary(gam1)
+par(mfrow=c(2,2)) 
+plot(gam1)
+par(mfrow=c(1,1)) 
+# dubbio su age
+
+lmg<-lm(drinks_day_modificati~0
+        +I(age)^3+gender+ 
+                +education+bmi+marital
+        #+insurance+private_insur+medicare+medicaid
+        +gen_health+iron, data=dati) 
+summary(lmg) 
+
+par(mfrow=c(2,2)) 
+plot(lmg)
+par(mfrow=c(1,1)) 
+
+bptest(lmg) #permane eteros
+#Inutili trasformate sulle x
+
+### model selection da rivedere ----
+library(MASS)
+step <- stepAIC(lmc, direction="both")
+
 
 ### Ultimo modello: metto factor delle variabili e uso GAM---
 lmf<-lm(log(drinks_day+1)~0
@@ -170,8 +239,7 @@ par(mfrow=c(1,1))
 car::ncvTest(lmf) #permane eteros. In ogni caso uso lm2
 
 # Controllo ipotesi
-library(gvlma)
-library(lmtest)
+
 #Prima
 bptest(lm1)
 gvlma(lm1)
@@ -179,6 +247,12 @@ gvlma(lm1)
 #Dopo
 bptest(lmf)
 gvlma(lmf) 
+
+### Confronto modello iniziale-finale ----
+pf<-predict(lmf,dati)
+par(mfrow=c(1,2)) 
+plot(p,dati$drinks_day)
+plot(pf, dati$drinks_day)
 
 
 ### logistico: drinkdays >= 52 come soglia critica ----
@@ -194,8 +268,23 @@ glm1<-glm(drink_dangerous~0
 summary(glm1)
 exp(glm1$coefficients)
 
-### Confronto modello iniziale-finale ----
-pf<-predict(lmf,dati)
-par(mfrow=c(1,2)) 
-plot(p,dati$drinks_day)
-plot(pf, dati$drinks_day)
+r2=1-(1305.0/2978.4) # null dev / resid
+r2
+
+library(coefplot)
+coefplot(fit, intercept=FALSE)
+
+# PREDICTION
+good_covariates$predicted_p <- predict(fit, good_covariates, type="response") 
+tail(good_covariates)
+
+# predicted target
+good_covariates$predicted_y <- ifelse(good_covariates$predicted_p > 0.5,1,0)
+
+table(observed=good_covariates$yes, predicted=good_covariates$predicted_y)/nrow(good_covariates)
+
+accuracy=0.315+0.4819
+accuracy
+
+
+
